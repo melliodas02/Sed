ff175190d57661177ce1740e54f8b591a15268e0
Melliodas02 <poseydonplay02@gmail.com>
2022-07-13 21:19:14 +0500

    update2

--- resources/views/sed/users/index.blade.php
+++ resources/views/sed/users/index.blade.php
@@ -32,7 +32,7 @@
                         <td>
                             @if(!empty($user->getRoleNames()))
                                 @foreach($user->getRoleNames() as $v)
-                                    <label class="badge badge-success">{{ $v }}</label>
+                                    <label class="">{{ $v }}</label>
                                 @endforeach
                             @endif
                         </td>
