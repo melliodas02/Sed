c13229ae2b6b5a43c7ee6a639be9c7986b0bb938
Melliodas02 <poseydonplay02@gmail.com>
2022-07-22 16:33:40 +0500

    Update22.07

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Pagination\Paginator;
 use Illuminate\Support\ServiceProvider;
 
 use Illuminate\Support\Facades\URL;
@@ -29,5 +30,6 @@ class AppServiceProvider extends ServiceProvider
 //        {
 //            \URL::forceScheme('https');
 //        }
+        Paginator::useBootstrap();
     }
 }
