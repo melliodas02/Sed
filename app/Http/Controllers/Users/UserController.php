819b7f61a9e1cb11f19884a59b2ffd5c3d4d629e
Melliodas02 <poseydonplay02@gmail.com>
2022-07-22 16:30:01 +0500

    Update22.07

--- app/Http/Controllers/Users/UserController.php
+++ app/Http/Controllers/Users/UserController.php
@@ -27,7 +27,7 @@ class UserController extends Controller
      */
     public function index(Request $request)
     {
-        $data = User::orderBy('id','DESC');
+        $data = User::orderBy('id','DESC')->paginate(5);
         return view('sed.users.index',compact('data'))
             ->with('i', ($request->input('page', 1) - 1) * 5);
     }
